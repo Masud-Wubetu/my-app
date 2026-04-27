504d2d2460822ce22992179135168aafa78d661f
Your Name <wubetumasud@gmail.com>
2026-04-28 02:20:28 +0300

    call the backend

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,17 +1,24 @@
+import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { FaArrowUp } from "react-icons/fa";
 import { Button } from './ui/button'
+import { useRef } from 'react';
 
 type FormData = {
     prompt: string
 }
 
 const ChatBot = () => {
+    const conversationId = useRef(crypto.randomUUID);
     const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
-    const onSubmit = (data: FormData) => {
-        console.log(data);
+    const onSubmit = async ({ prompt }: FormData) => {
         reset();
+        const {data} = await axios.post('/api/chat', {
+            prompt,
+            conversationId: conversationId.current
+        });
+        console.log(data)
     }
 
     const onKeyDown = (e:React.KeyboardEvent<HTMLFormElement>) => {
